1ed7ae514bd2a6febcb53174906c27d6fd0ad64a
olu oyelaja <oyelajaos@gmail.com>
2020-08-18 09:47:56 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -11,16 +11,24 @@ function App() {
         <h1>MUZIKI</h1>
         <h2>...refreshing the SOUL!</h2>
       </div>
-      <div>
-      <ul className="nav">
+        <div>
+            <ul className="nav">
                 <li><a href="#">Download</a></li>
                 <li><a href="#">Licensing</a></li>
                 <li><a href="#">Subscription</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Create Account | Sign in</a></li>
             </ul>
+        </div>
+        <div>
+        <img className= "img" src="/images/a1.jpg" alt="img" />
+        <img className= "imgb" src="/images/b1.jpg" alt="img" />
+        <img className= "img" src="/images/e1.jpg" alt="img" />
+        <img className= "imgb" src="/images/c1.jpg" alt="img" />
+        </div>
+        <div className="display">
 
-      </div>
+        </div>
     </div>
   );
 }
