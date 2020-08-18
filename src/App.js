7653f3fbb9e69f8af925af7099d3028de53df855
olu oyelaja <oyelajaos@gmail.com>
2020-08-18 08:16:57 -0400

    modified

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-        
         <h1>MUZIKI</h1>
         <h2>...refreshing the SOUL!</h2>
-        
-           
+      </div>
+      <div>
+      <ul className="nav">
+                <li><a href="#">Download</a></li>
+                <li><a href="#">Licensing</a></li>
+                <li><a href="#">Subscription</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Create Account | Sign in</a></li>
+            </ul>
+
       </div>
     </div>
   );
