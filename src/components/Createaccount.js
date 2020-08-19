2d266ebad09c5840a939aff132f592fd761eac77
olu oyelaja <oyelajaos@gmail.com>
2020-08-19 10:16:41 -0400

    changes

--- src/components/Createaccount.js
+++ src/components/Createaccount.js
@@ -1,39 +1,45 @@
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom'
 
 
 
-class Createaccount extends React.Component {
-    constructor(props){
-        super(props);
-            this.state = {
-                value:''
-            }    
-    }
-    handleChange = (event) =>{
-        this.setState ({value:event.target.value});
-    }
-    render() {
-        return (
-            <div class="reg-form">
-                
-                <form>
-                <h1 class="form-reg">Register Here!</h1>
-                    <label>
-                        <h3>First Name</h3>
-                        <input type='text' placeholder="First Name" value={this.handleChange.change}
-                        onChange={this.state.value} />
-                    </label>
-                    <label>
-                        <h3>Password</h3>
-                        <input type='password' placeholder="Password" value={this.handleChange.change}
-                        onChange={this.state.value} />
-                    </label>
-                    <br></br><br></br>
-                        <input class="btn-submit" type='Submit' value='Click Here to Submit'/>
-                </form>
-            </div>
-        )
-    }
+
+
+function Createaccount() {
+  return (
+    <div className="App-wrapper">
+      <div className="Header-top">
+        <h1>MUZIKI</h1>
+        <h2>...refreshing the SOUL!</h2>
+      </div>
+        <div>
+            <ul className="nav-bar">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Listentomusic">Listen to Music</Link></li>
+                <li><Link to="/Licensing">Licensing</Link></li>
+                <li><Link to="/Subscription">Subscription</Link></li>
+                <li><Link to="/Contact">Contact</Link></li>
+                <li><Link to="/Createaccount">Create Account</Link></li>
+                <li><Link to="/Signin">Sign in</Link></li>
+            </ul>
+        </div>
+        <div className="display-songs">
+            <h3>CONTACT</h3>
+            <h4>
+           MUZIKI INC<br></br>Muziki Plaza New York NY 10001<br></br>347 222 5252<br></br>info@muziki.com<br></br>www.muziki.com
+            </h4>
+
+        </div>
+        <div>
+          <img className= "img-display" src="/images/a1.jpg" alt="img" />
+          <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
+          <img className= "img-display" src="/images/e1.jpg" alt="img" />
+          <img className= "imgb-display" src="/images/c1.jpg" alt="img" />
+        </div>
+    </div>
+  );
 }
 
 export default Createaccount;
