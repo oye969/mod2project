7177f1432bc1b9ce29b43389c1d358074f80ded5
olu oyelaja <oyelajaos@gmail.com>
2020-08-18 22:42:29 -0400

    changes

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <h1>MUZIKI</h1>
+        <h2>...refreshing the SOUL!</h2>
+      </div>
+        <div>
+            <ul className="nav">
+                <li><a href="#">Download</a></li>
+                <li><a href="#">Licensing</a></li>
+                <li><a href="#">Subscription</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Create Account</a></li>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </div>
+        <div>
+          <img className= "img" src="/images/a1.jpg" alt="img" />
+          <img className= "imgb" src="/images/b1.jpg" alt="img" />
+          <img className= "img" src="/images/e1.jpg" alt="img" />
+          <img className= "imgb" src="/images/c1.jpg" alt="img" />
+        </div>
+        <div className="display">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
