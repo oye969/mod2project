4754478e47abbe0c99f0a700a97aefd08356049d
olu oyelaja <oyelajaos@gmail.com>
2020-08-19 09:09:08 -0400

    changes

--- src/components/Licensing.js
+++ src/components/Licensing.js
@@ -25,14 +25,18 @@ function Licensing() {
                 <li><Link to="/Signin">Sign in</Link></li>
             </ul>
         </div>
+        <div className="display-songs">
+            <h3>LICENSING</h3>
+            <h4>
+            Performance licenses, including blanket licenses, are considered nonexclusive. This means that a PRO can give out licenses for the same catalogue of music to multiple organizations. In some cases, a copyright holder may decide to negotiate exclusivity with their PRO, or a business can negotiate a license for a specific musician’s or composer’s work directly with the artist. However, access to a wide range of music that can be chosen by the business owner based on mood, brand, and personal taste is one important reason PROs remain in charge of copyrights.
+            </h4>
+
+        </div>
         <div>
           <img className= "img-display" src="/images/a1.jpg" alt="img" />
           <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
           <img className= "img-display" src="/images/e1.jpg" alt="img" />
           <img className= "imgb-display" src="/images/c1.jpg" alt="img" />
-        </div>
-        <div className="display-songs">
-
         </div>
     </div>
   );
--- src/components/Listentomusic.js
+++ src/components/Listentomusic.js
@@ -22,6 +22,7 @@ class Listentomusic extends React.Component {
   render() {
     return (
         <div>
+          
 
         </div>
     );
