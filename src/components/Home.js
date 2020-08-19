f07f1b33784ce2b7008be3a93af192eda09e6840
olu oyelaja <oyelajaos@gmail.com>
2020-08-19 08:21:28 -0400

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
   Link,
-  Redirect
 } from 'react-router-dom'
 
 
---
+++ src/components/Subscription.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Link,
+} from 'react-router-dom'
+
+
+
+
+
+function Subscription() {
+  return (
+    <div className="App-wrapper">
+      <div className="Header-top">
+        <h1>MUZIKI</h1>
+        <h2>...refreshing the SOUL!</h2>
+      </div>
+        <div>
+            <ul className="nav-bar">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Listentomusic">Listen to Music</Link></li>
+                <li><Link to="/Licensing">Licensing</Link></li>
+                <li><Link to="/Subscription">Subscription</Link></li>
+                <li><Link to="/Contact">Contact</Link></li>
+                <li><Link to="/Createaccount">Create Account</Link></li>
+                <li><Link to="/Signin">Sign in</Link></li>
+            </ul>
+        </div>
+        <div>
+          <img className= "img-display" src="/images/a1.jpg" alt="img" />
+          <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
+          <img className= "img-display" src="/images/e1.jpg" alt="img" />
+          <img className= "imgb-display" src="/images/c1.jpg" alt="img" />
+        </div>
+        <div className="display-songs">
+
+        </div>
+    </div>
+  );
+}
+
+export default Subscription;
