b267e9a851f3d25a4c8ccd28af63f06955f1a568
olu oyelaja <oyelajaos@gmail.com>
2020-08-18 23:15:31 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,4 +2,26 @@ import React from 'react';
 import '.App.css';
 import {
     BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom'
+import Home from './components/Home'
+import Home from './components/Contact';
+import Home from './components/Createaccount';
+import Home from './components/Download';
+import Home from './components/Licensing';
+import Home from './components/Subscription';
+import Home from './components/Signin';
+
+
+function App() {
+    return(
+        <Router>
+            <div>
+                
+            </div>
+        </Router>
+    )
 }
