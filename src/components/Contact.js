8c68e4db237257773b697a2d932d903678b29d34
olu oyelaja <oyelajaos@gmail.com>
2020-08-20 19:53:28 -0400

    changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,30 +14,29 @@ function Contact() {
         <h1>MUZIKI</h1>
         <h2>...refreshing the SOUL!</h2>
       </div>
-        <div>
-            <ul className="nav-bar">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Listentomusic">Listen to Music</Link></li>
-                <li><Link to="/Licensing">Licensing</Link></li>
-                <li><Link to="/Subscription">Subscription</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-                <li><Link to="/Createaccount">Create Account</Link></li>
-                <li><Link to="/Signin">Sign in</Link></li>
-            </ul>
-        </div>
-        <div className="display-songs">
+      <div>
+        <ul className="nav-bar">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Listentomusic">Listen to Music</Link></li>
+          <li><Link to="/Licensing">Licensing</Link></li>
+          <li><Link to="/Subscription">Subscription</Link></li>
+          <li><Link to="/Contact">Contact</Link></li>
+          <li><Link to="/Createaccount">Create Account</Link></li>
+          <li><Link to="/Signin">Sign in</Link></li>
+        </ul>
+      </div>
+      <div className="display-songs">
             <h3>CONTACT</h3>
             <h4>
-           MUZIKI INC<br></br>Muziki Plaza New York NY 10001<br></br>347 222 5252<br></br>info@muziki.com<br></br>www.muziki.com
+              MUZIKI INC<br></br>Muziki Plaza New York NY 10001<br></br>347 222 5252<br></br>info@muziki.com<br></br>www.muziki.com
             </h4>
-
-        </div>
-        <div>
-          <img className= "img-display" src="/images/a1.jpg" alt="img" />
-          <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
-          <img className= "img-display" src="/images/e1.jpg" alt="img" />
-          <img className= "imgb-display" src="/images/c1.jpg" alt="img" />
-        </div>
+      </div>
+      <div>
+        <img className= "img-display" src="/images/a1.jpg" alt="img" />
+        <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
+        <img className= "img-display" src="/images/e1.jpg" alt="img" />
+        <img className= "imgb-display" src="/images/c1.jpg" alt="img" />
+      </div>
     </div>
   );
 }
