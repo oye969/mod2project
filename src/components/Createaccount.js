063d3b1ef8cd84f864321ed87acac3fdcbcaca34
olu oyelaja <oyelajaos@gmail.com>
2020-08-20 19:57:55 -0400

    changes

--- src/components/Createaccount.js
+++ src/components/Createaccount.js
@@ -15,21 +15,20 @@ function Createaccount() {
         <h2>...refreshing the SOUL!</h2>
       </div>
         <div>
-            <ul className="nav-bar">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Listentomusic">Listen to Music</Link></li>
-                <li><Link to="/Licensing">Licensing</Link></li>
-                <li><Link to="/Subscription">Subscription</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-                <li><Link to="/Createaccount">Create Account</Link></li>
-                <li><Link to="/Signin">Sign in</Link></li>
-            </ul>
+          <ul className="nav-bar">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Listentomusic">Listen to Music</Link></li>
+            <li><Link to="/Licensing">Licensing</Link></li>
+            <li><Link to="/Subscription">Subscription</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+            <li><Link to="/Createaccount">Create Account</Link></li>
+            <li><Link to="/Signin">Sign in</Link></li>
+          </ul>
         </div>
-        <div className="display-songs">
+      <div className="display-songs">
         <h3>CREATE ACCOUNT</h3>
-            <h4>
-            To create an account you need a valid email address. To receive text messages you need to a valid mobile phone number. Select the topics you're interested in and we'll send you the updates. You'll receive a confirmation notice afterwards, and you can easily update your preferences anytime.
-            </h4>
+        <h4>To create an account you need a valid email address. To receive text messages you need to a valid mobile phone number. Select the topics you're interested in and we'll send you the updates. You'll receive a confirmation notice afterwards, and you can easily update your preferences anytime.
+        </h4>
             <button> Create Account</button>
 
         </div>
