eb7e275e64a85842991a8dedcbcd3ed226049be4
olu oyelaja <oyelajaos@gmail.com>
2020-08-20 20:02:09 -0400

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,17 +14,17 @@ function Home() {
         <h1>MUZIKI</h1>
         <h2>...refreshing the SOUL!</h2>
       </div>
-        <div>
-            <ul className="nav-bar">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Listentomusic">Listen to Music</Link></li>
-                <li><Link to="/Licensing">Licensing</Link></li>
-                <li><Link to="/Subscription">Subscription</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-                <li><Link to="/Createaccount">Create Account</Link></li>
-                <li><Link to="/Signin">Sign in</Link></li>
-            </ul>
-        </div>
+      <div>
+          <ul className="nav-bar">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Listentomusic">Listen to Music</Link></li>
+            <li><Link to="/Licensing">Licensing</Link></li>
+            <li><Link to="/Subscription">Subscription</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+            <li><Link to="/Createaccount">Create Account</Link></li>
+            <li><Link to="/Signin">Sign in</Link></li>
+          </ul>
+      </div>
         <div>
           <img className= "img-display" src="/images/a1.jpg" alt="img" />
           <img className= "imgb-display" src="/images/b1.jpg" alt="img" />
